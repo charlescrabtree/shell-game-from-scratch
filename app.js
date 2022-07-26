0cf1173f7be93ed13368f536618c2d23b56d6ce8
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-07-26 15:53:36 -0700

    html and basic css. Need new image for wood door

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // import functions and grab DOM elements
+// const woodDoor = document.getElementById('wood-door');
+// const stoneDoor = document.getElementById('stone-door');
+// const vaultDoor = document.getElementById('vault-door');
+
+const woodButton = document.getElementById('wood-button');
+const stoneButton = document.getElementById('stone-button');
+const vaultButton = document.getElementById('vault-button');
+
+const adoptionsEl = document.getElementById('adoptions');
+const discardsEl = document.getElementById('discards');
+const totalEl= document.getElementById('total');
+
+const hidingSpots = ['wood-door', 'stone-door', 'vault-door'];
 
 // let state
+let adoptions = 0
+let discards = 0
+let total = 0
 
-// set event listeners 
+// set event listeners
+
+woodButton.addEventListener('click', )
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
