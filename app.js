9c7031a18c5f89d84028089f83369a7e5061d864
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-07-29 14:53:53 -0700

    it works!

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const woodButton = document.getElementById('wood-button');
 const stoneButton = document.getElementById('stone-button');
 const vaultButton = document.getElementById('vault-button');
-// const adoptionsEl = document.getElementById('adoptions');
-// const discardsEl = document.getElementById('discards');
-// const totalEl = document.getElementById('total');
-const hidingSpots = ['wood-door', 'stone-door', 'vault-door'];
+
+
 const woodImg = document.getElementById('wood-img');
 const stoneImg = document.getElementById('stone-img');
 const vaultImg = document.getElementById('vault-img');
+const hidingSpots = [woodImg, stoneImg, vaultImg];
 const fateEl = document.getElementById('fate');
 
 
@@ -21,53 +20,49 @@ function resetImages() {
     vaultImg.src = './assets/vault-door.jpg';
 }
 
-// function displayAdoptsDiscAndTot() {
-//     adoptionsEl.textContent = adoptions;
-//     discardsEl.textContent = total - adoptions;
-//     totalEl.textContent = total;
-
-// }
-
 woodButton.addEventListener('click', () => {
     resetImages();
     total++;
-    handleGuess(0);
-    // const hidingSpot = Math.floor(Math.random() * 3);
-    // const answer = hidingSpots[hidingSpot];
-    // handleGuess(answer, 'wood-door');
+    const hidingSpot = Math.floor(Math.random() * 3);
+    const answer = hidingSpots[hidingSpot];
+
+    handleGuess(answer, 'wood-img');
 });
 
 stoneButton.addEventListener('click', () => {
     resetImages();
     total++;
-    handleGuess(2);
-    // const hidingSpot = Math.floor(Math.random() * 3);
-    // const answer = hidingSpots[hidingSpot];
-    // handleGuess(answer, 'stone-door');
+
+    const hidingSpot = Math.floor(Math.random() * 3);
+    
+    const answer = hidingSpots[hidingSpot];
+    
+    handleGuess(answer, 'stone-img');
 });
 
 vaultButton.addEventListener('click', () => {
     resetImages();
     total++;
-    handleGuess(1);
-    // const hidingSpot = Math.floor(Math.random() * 3);
-    // const answer = hidingSpots[hidingSpot];
-    // handleGuess(answer, 'vault-door');
+
+    const hidingSpot = Math.floor(Math.random() * 3);
+    
+    const answer = hidingSpots[hidingSpot];
+    
+    handleGuess(answer, 'vault-img');
 });
 
 
 
 function handleGuess(correctSpot, userGuess) {
-    const randomIndex = Math.floor(Math.random() * hidingSpots.length);
-    const randomDoorImg = hidingSpots[randomIndex];
 
-    randomDoorImg.src = 'assets/baby-dragon.jpg';
+    correctSpot.src = './assets/baby-dragon.jpg';
 
-    if (randomIndex === userGuess) {
+    if (correctSpot.id === userGuess) {
         adoptions++;
     } else {
         discards++;
     }
+    
     fateEl.textContent = `You've adopted ${adoptions} baby dragon(s), discarded ${discards} baby dragons, and handled the fate of ${total} dragon(s). You monster. Try again?`;
     
 }
